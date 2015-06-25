221586482782e3a6a8a9810c27e3f82ca256c953
Petr Viktorin <encukou@gmail.com>
2015-06-25 12:06:04 +0200

    A simple Flask app

---
+++ setup.py
@@ -0,0 +1,48 @@
+
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        super().finalize_options()
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup_args = dict(
+    name='pyvocz',
+    version='0.0.1',
+    packages=['pyvocz'],
+
+    description="""Website with information about Pyvo meetups (pyvo.cz)""",
+    author='Petr Viktorin',
+    author_email='encukou@gmail.com',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    install_requires=[
+        'pyvodb',
+        'flask >= 0.10, < 1.0',
+    ],
+
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+)
+
+
+if __name__ == '__main__':
+    setup(**setup_args)
